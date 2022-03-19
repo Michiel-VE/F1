22372f37c9a3d6761594f24a7290149bb15bdde7
Michiel_VE <r0794174@student.thomasmore.be>
2022-03-19 10:09:21 +0100

    adding lint and setup interface KEEP TODO'S IN MIND

---
+++ src/interfaces/driver.ts
@@ -0,0 +1,14 @@
+export interface Driver{
+  id: number;
+  team_id: number;
+  firstname: string;
+  lastname: string;
+  permanentNumber: string;
+  code: string;
+  birthday: Date;
+  picture: string;
+  country: string;
+  wins: string;
+  points: number;
+  penaltyPoints: number;
+}
---
+++ src/interfaces/race.ts
@@ -0,0 +1,11 @@
+export interface Race{
+  id: number;
+  name: string;
+  lat: number;
+  lon: number;
+  city: string;
+  country: string;
+  date: Date;
+  timezone: string;
+  active: boolean;
+}
