528de3efc9218292b7ea3db1e9c22835666230e1
Michiel-VE <r0794174@student.thomasmore.be>
2022-04-25 19:56:12 +0200

    fixed tests

--- src/app/details/driver/driver.component.spec.ts
+++ src/app/details/driver/driver.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ActivatedRoute } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { DriverComponent } from './driver.component';
 
@@ -9,6 +12,7 @@ describe('DriverComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ DriverComponent ],
+      imports: [HttpClientModule, RouterTestingModule],
     })
     .compileComponents();
   });
